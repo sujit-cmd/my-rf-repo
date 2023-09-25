9078de2c8e3c1adde8f026d324687ad51a316264
sujit-student <sujit341@gmail.com>
2023-09-25 21:56:42 +0530

    2nd commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "bucket" {
   source = "github.com/terraform-google-modules/terraform-google-cloud-storage.git/modules/simple_bucket"
 
-  name       = "${var.project_id}-bucket"
+  name       = "${var.project_id}-bucket-1"
   project_id = var.project_id
   location   = "us"
 }
